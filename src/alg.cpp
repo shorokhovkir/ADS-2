ef9a79dd8cd75e4616798a4871f2f5fc64daa806
shorokhovkir <160240169+shorokhovkir@users.noreply.github.com>
2024-03-03 13:39:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,40 @@
 
 
 double pown(double value, uint16_t n) {
-
+  double res = 1.0;
+  for (uint16_t i = 1; i <= n; i++)
+    res *= value;
+  return res;
 }
 
 uint64_t fact(uint16_t n) {
-
+  uint64_t res = 1;
+  for (uint16_t i = 1; i <= n; i++)
+    res *= i;
+  return res;
 }
 
 double calcItem(double x, uint16_t n) {
-
+  return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-
+  double res = 0.0;
+  for (uint16_t i = 0; i <= count; i++)
+    res += calcItem(x, i);
+  return res;
 }
 
 double sinn(double x, uint16_t count) {
-
+  double res = 0.0;
+  for (uint16_t i = 1; i <= count; i++)
+    res += pown(-1, i - 1) * calcItem(x, 2 * i - 1);
+  return res;
 }
 
 double cosn(double x, uint16_t count) {
-
+  double res = 0.0;
+  for (uint16_t i = 1; i <= count; i++)
+    res += pown(-1, i - 1) * calcItem(x, 2 * i - 2);
+  return res;
 }
